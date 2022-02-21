f7c893cce217ca9764e7dfe462b88e626241d1e6
drili <DriliB@hotmail.com>
2022-02-21 14:37:35 +0100

    :sparkles: - Visual Studio Git Test

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({allPostsData}) {
       <section className={stylesUtil.headingMd}>
         <p>Hello, my name is <b>Drilon Braha</b>, I'm currently learning Next.js</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - Testing you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
